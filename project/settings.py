d1d542c5611ef06f37abf5fc65a4636726b05f62
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-10-01 14:03:29 -0400

    Create the snippets model

--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,10 +77,16 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : 'django.db.backends.postgresql',
+        'NAME' : 'postgres',
+        'HOST' : os.environ.get('SUPABASE_HOST'),
+        'PASSWORD': os.environ.get('SUPABASE_PASSWORD'),
+        'PORT': os.environ.get('SUPABASE_PORT'),
+        'USER': os.environ.get('SUPABASE_USER'),
+        'CERT' : os.environ.get('SUPABASE_CERT'),
     }
 }
+
 
 
 # Password validation
