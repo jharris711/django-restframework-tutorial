d7eb61d531ffcf49ca3c5b46608e990cde957984
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-10-01 22:42:46 -0400

    create root and highlighted snippets endpoints

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
-    path('', include('snippets.urls')),
+    path('', views.api_root),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
     path('api-auth/', include('rest_framework.urls')),
 ]
